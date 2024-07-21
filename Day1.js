fbc1d74dbad29588341371a474e5827dece70524
Sandhya-jam <jamsandhyarani@gmail.com>
2024-07-21 08:52:06 +0530

    added day1.js file

---
+++ Day1.js
@@ -0,0 +1,35 @@
+//ACTIVITY-1:using var & let
+var a=10;
+console.log(a);
+let str="sandhya"
+console.log(str);
+
+//ACTIVITY-2:using const
+const flag=true;
+console.log(flag);
+
+//ACTIVITY-3:data-types
+var b=10;
+let Bsf="Ashu";
+let possible=false;
+arr=[1,2,3,4];
+const obj={
+    name:'Sandhya Rani',
+    branch:'CSE',
+    age:18
+};
+console.log(typeof(b))
+console.log(typeof(Bsf))
+console.log(typeof(possible))
+console.log(typeof(arr))
+console.log(typeof(obj))
+
+// ACTIVITY-4:Re-assigning
+let x=20
+console.log(x)
+x=10
+console.log(x)
+
+//ACTIVITY-5:understanding const
+const z=9
+// z=12 this cant be done because const cant be changed once value is assigned
